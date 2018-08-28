6fd9bef39456f6af81dedea9052ff9065e86a2dd
Bruno Azoulay <azoulaybruno@gmail.com>
2018-08-28 12:14:20 +0200

    Font & file-loader
    add file-loader to use file image and font

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ let config = {
 		rules: [{
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.(png|svg|jpg|jpeg|gif)$/,
+			use: ['file-loader']
+		}, {
+			test: /\.(woff|woff2|eot|ttf|otf)$/,
+			use: ['file-loader']
 		}]
 	}
 
