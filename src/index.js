dffe0502a17485c75ddcf167047d1b4f96e5f5ac
Bruno Azoulay <azoulaybruno@gmail.com>
2018-08-28 11:19:48 +0200

    webpack css-loader
    webpack at use css loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-alert('hello')
+import './css/style.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const {
-    resolve
+	resolve
 } = require('path')
 
 let config = {
-    mode: 'none',
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: resolve(__dirname, './dist')
-    }
+	mode: 'none',
+	entry: {
+		app: './src/index.js'
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: resolve(__dirname, './dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}]
+	}
 
 }
+
+module.exports = config
